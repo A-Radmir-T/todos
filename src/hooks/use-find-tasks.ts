7359f24a6924a667f60ae9acfb584221f59a51b7
A-Radmir-T <131308803+A-Radmir-T@users.noreply.github.com>
2023-09-15 21:06:35 +0500

    Update use-find-tasks.ts

--- src/hooks/use-find-tasks.ts
+++ src/hooks/use-find-tasks.ts
@@ -1,9 +1,6 @@
 import { useState } from 'react'
 import { useDebounce } from './use-debounce'
 
-export const useFindTasks = (value: string, delay: number = 500) => {
-	const [text, setText] = useState('')
-	const debouncedValue = useDebounce(value, delay)
-
-	return debouncedValue
+export const useFindTasks = () => {
+	
 }
